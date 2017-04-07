88879cd7876c128d9bafa656c5049cc0eb0e4b32
maduc7 <maduc7@gmail.com>
2017-04-07 09:22:32 -0400

    addaptation for feature selection with SVD entropy

--- test_hit_selection_old.R
+++ test_hit_selection_old.R
@@ -11,10 +11,10 @@ library(dplyr)
 library(ggplot2)
 
 
-files <- c("Pf_Gustafsdottir.rds", "Pf_Gustafsdottir_fs.rds")
-feat.sel <- c(FALSE, TRUE)
-n.feat <- c(50, 30)
-seeds <- seq(from = 1, to = 200, by = 1)
+files <- "Pf_Gustafsdottir_fs_svd.rds" #c("Pf_Gustafsdottir.rds", "Pf_Gustafsdottir_fs.rds")
+feat.sel <- TRUE #c(FALSE, TRUE)
+n.feat <- 30 #c(50, 30)
+seeds <- seq(from = 1, to = 100, by = 1)
 
 hit.ratio <- c()
 
@@ -30,9 +30,13 @@ for(i in 1:length(files)){
   }
 }
 
-test <- data.frame(seed = rep(seeds, 4), 
-                   hit.ratio = hit.ratio[1,], 
-                   methods = rep(c("Pearson", "Pearson with feat. sel", "Jaccard", "Jaccard with feat. sel"), each = length(seeds)))
+#test <- data.frame(seed = rep(seeds, 4), 
+#                   hit.ratio = hit.ratio[1,], 
+#                   methods = rep(c("Pearson", "Pearson with feat. sel", "Jaccard", "Jaccard with feat. sel"), each = length(seeds)))
+
+test <- data.frame(seed = rep(seeds, 2), 
+                   hit.ratio = hit.ratio[1,],
+                   methods = rep(c("Pearson with feat sel SVD-entropy", "Jaccard with feat sel SVD-entropy"), each = length(seeds)))
 
 hit_ratio_vs_seed_plot <- ggplot(data = test, aes(x=methods, y=hit.ratio)) + 
   geom_boxplot() + 
@@ -40,4 +44,4 @@ hit_ratio_vs_seed_plot <- ggplot(data = test, aes(x=methods, y=hit.ratio)) +
   labs(y = "hit ratio") +
   ylim(0, 1)
 
-ggsave(filename = 'hit_ratio_vs_seed.png', plot = hit_ratio_vs_seed_plot, width = 6, height = 6)
+ggsave(filename = 'hit_ratio_vs_seed_svd.png', plot = hit_ratio_vs_seed_plot, width = 6, height = 6)

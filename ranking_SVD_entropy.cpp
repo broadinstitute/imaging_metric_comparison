492e82a053faf23db5681a5d9b7d4ccd92b53d2b
maduc7 <maduc7@gmail.com>
2017-04-12 14:51:59 -0400

    FS2 method added

--- ranking_SVD_entropy.cpp
+++ ranking_SVD_entropy.cpp
@@ -26,8 +26,15 @@ double calculate_entropy(mat A) {
 	return E;
 }
 
+/* SR method
+Simple Ranking:
+ select mc features according to the highest ranking order of their CE values
+ */
+//' Calculate the entropy contribution of a matrix based on SVD
+//' @param A dataset mxn (m features and n observations) 
+//' @return entropy contribution vector
 // [[Rcpp::export]]
-NumericVector CE_entropy(NumericMatrix A){
+NumericVector CE_entropy_SR(NumericMatrix A){
   // convert into matrix (armadillo)
   mat Amat(A.begin(), A.nrow(), A.ncol(), false);
   // total entropy
@@ -46,34 +53,94 @@ NumericVector CE_entropy(NumericMatrix A){
 	
 	return CE;
 }
-  /*mat test(3,3);
-  test << 1 << 1 << 1 << endr
-       << 2 << 2 << 2 << endr
-       << 3 << 3 << 3 << endr;
-  //<< 4 << 4 << 4 << endr;
-  
-  mat testt  = test * test.t();
-  
-  testt.print("Testt: ");
-  
-  vec s = svd(test);
-  
-  s.print("s: ");
-  
-  vec v = s/sum(s); 
-  v.print("v: ");
-  uvec idxZer0 = find(v < 0.5);
-  idxZer0.print("v: ");
-  int l = idxZer0.size();
-  cout << l << endl;
-  
-  
-  vec t(4);
-  t << 2 << 3 << 4 << 5;
-  arma::vec vNoZero = t.elem(find(t >= 0));
-  double E = -sum(vNoZero % log(vNoZero))/log(vNoZero.size()); 
-  cout << E << endl;*/
 
-/*
-A <- profiles %>% select(one_of(variables)) %>% as.matrix() %>% t(.)
+/* FS2 method
+Forward selection
 */
+//' Calculate the entropy of a matrix based on SVD with FS method
+//' @param A dataset mxn (m features and n observations) 
+//' @return index of features that were selected
+// [[Rcpp::export]]
+NumericVector CE_entropy_FS2(NumericMatrix A, unsigned int mc){
+  // convert into matrix (armadillo)
+  mat Amat(A.begin(), A.nrow(), A.ncol(), false);
+  
+  // vector of index of the features
+  vec idxFeat(linspace<vec>(1, Amat.n_rows, Amat.n_rows));
+  
+  // best features index
+  NumericVector idxBest(mc);
+  
+  for(unsigned int j = 0; j < mc; j++){
+    // total entropy
+    double E = calculate_entropy(Amat);
+    
+    // Contribution vector to the entropy by a leave-one-out comparison
+    vec CE(Amat.n_rows);
+    
+    // for each feature calculate the contribution to the entropy by a leave-one-out comparison
+    for(unsigned int i = 0; i < Amat.n_rows; i++){
+      mat Ai = Amat;
+      // remove row i
+      Ai.shed_row(i);
+      double Ei = calculate_entropy(Ai);
+      CE[i] = E - Ei;
+    }
+    // find the index of the highest entropy contribution
+    int idx = CE.index_max();
+    idxBest[j] = idxFeat[idx];
+    
+    // remove the best feature
+    Amat.shed_row(idx);
+    idxFeat.shed_row(idx);
+  }
+  cout << "Best features: " << idxBest << endl;
+  
+  return idxBest;
+}
+
+/* BE method
+ Backward Elimination
+ Super expensive method...
+*/
+//' Calculate the entropy of a matrix based on SVD with BE method
+//' @param A dataset mxn (m features and n observations) 
+//' @return index of features that were selected
+// [[Rcpp::export]]
+NumericVector CE_entropy_BE(NumericMatrix A, unsigned int mc){
+  // convert into matrix (armadillo)
+  mat Amat(A.begin(), A.nrow(), A.ncol(), false);
+  
+  // vector of index of the features
+  vec idxFeat(linspace<vec>(1, Amat.n_rows, Amat.n_rows));
+  
+  // best features index
+  //NumericVector idxBest(mc);
+  
+  for(unsigned int j = 0; j < Amat.n_rows-mc; j++){ // TODO: test that this is correct
+    // total entropy
+    double E = calculate_entropy(Amat);
+    
+    // Contribution vector to the entropy by a leave-one-out comparison
+    vec CE(Amat.n_rows);
+    
+    // for each feature calculate the contribution to the entropy by a leave-one-out comparison
+    for(unsigned int i = 0; i < Amat.n_rows; i++){
+      mat Ai = Amat;
+      // remove row i
+      Ai.shed_row(i);
+      double Ei = calculate_entropy(Ai);
+      CE[i] = E - Ei;
+    }
+    // find the index of the highest entropy contribution
+    int idx = CE.index_min();
+    //idxBest[j] = idxFeat[idx];
+    
+    // remove the best feature
+    Amat.shed_row(idx);
+    idxFeat.shed_row(idx);
+  }
+  cout << "Best features: " << idxFeat << endl;
+  
+  return wrap(idxFeat);
+}

53b8db9c858f110b8526bd38f2ff6d462dc2c790
maduc7 <maduc7@gmail.com>
2017-07-05 20:26:18 -0400

    create a profile (mean of the replicate)

---
+++ profile_generator.R
@@ -0,0 +1,33 @@
+# all usefull libraries
+library(magrittr)
+library(dplyr)
+
+#' Function to do profiles of the compounds
+#' @param pf the data file
+#' @return profiles
+
+profile_generator <- function(pf){ 
+
+  # Metadata
+  metadata <-
+    names(pf) %>% str_subset("^Metadata_")
+  
+  # Variables
+  variables <-
+    names(pf) %>% str_subset("^Cells_|^Cytoplasm_|^Nuclei_")
+
+  # average along replicate (keep only id and variables)
+  signature <- 
+    pf %>% 
+    select(one_of(variables, 'Metadata_broad_sample')) %>%
+    group_by(Metadata_broad_sample) %>% 
+    summarise_each(funs(mean(., na.rm=TRUE)))
+  
+  # add metadata information
+  signature %<>%
+    left_join(., pf %>% select(one_of(metadata)), by = 'Metadata_broad_sample') %>%
+    group_by(Metadata_broad_sample) %>%
+    slice(1)
+  
+  return(signature)
+}
